54f2b806db81ae7beacaab6f05f166e65b7dac1d
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-10-21 16:32:07 +0430

    style issue status cards

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -6,7 +6,7 @@ import {BookOpenIcon, UserIcon, UsersIcon} from "@heroicons/react/16/solid";
 const HomePage = () => {
     return (
         <div>
-            <div className={"flex items-center gap-x-4 px-8 mb-4"}>
+            <div className={"flex items-center gap-x-4 px-2 md:px-8 mb-4"}>
                 <StatusCard title={"Active Students"} count={200}
                             Icon={UsersIcon}/>
                 <StatusCard title={"Active Teachers"} count={36}
