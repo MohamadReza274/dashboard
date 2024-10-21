5c271e9ec9e32f7b8e9c3629448bd880ef623d24
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-10-21 16:34:21 +0430

    issue with style

--- src/Components/StatusCard.tsx
+++ src/Components/StatusCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const StatusCard = ({title,count,Icon}:Props) => {
     return (
-        <div className="flex flex-col py-2 md:px-4 max-w-48 md:max-w-md w-full bg-gray-50 rounded-lg shadow gap-y-4">
+        <div className="flex flex-col py-2 px-4 max-w-48 md:max-w-md w-full bg-gray-50 rounded-lg shadow gap-y-4">
             <h2 className={"text-gray-700"}>{title}</h2>
             <div className={"flex gap-x-2"}>
                 <Icon className={"h-6 w-6 outline p-1 outline-1 rounded-full"} />
